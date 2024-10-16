05de427ee29a405e33045499bdb5e33484670bf4
tmpelay <tmpelay@gmail.com>
2024-10-16 15:31:26 -0300

    Change CORS origin to deploy on Vercel - fix 1

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://linces-voley-sales-front.vercel.app/",
+    origin: "https://linces-voley-sales-front.vercel.app",
     credentials: true,
   })
 );
