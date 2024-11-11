7c3e9b03dc6b1cafe67291f1a2b627870a73f5ee
tmpelay <tmpelay@gmail.com>
2024-11-11 02:12:25 -0300

    Create sale route

---
+++ prisma/migrations/20241111051153_sale/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Sale" ALTER COLUMN "active" SET DEFAULT true;
--- src/controllers/sale.controllers.js
+++ src/controllers/sale.controllers.js
@@ -22,3 +22,22 @@ export const getSale = async (req, res) => {
     return res.status(404).json({ message: "Sale not found" });
   }
 };
+
+export const createSale = async (req, res) => {
+  try {
+    const data = req.body;
+
+    const newSale = await prisma.sale.create({
+      data: {
+        dozenPrice: data.dozen_price,
+        halfDozenPrice: data.half_dozen_price,
+        date: data.date,
+      },
+    });
+
+    return res.json(newSale);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
