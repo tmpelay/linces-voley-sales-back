d2a670bb9caaf7c7540730d833aecdfda0088b55
tmpelay <tmpelay@gmail.com>
2024-11-11 00:46:40 -0300

    Fix CORS origin 2

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://linces-voley-sales-back.onrender.com",
+    origin: "https://linces-voley-sales-back.onrender.com/",
     credentials: true,
   })
 );
