c6ca4122b96f5bef14f26d1dc19a48906a1ddbdd
tmpelay <tmpelay@gmail.com>
2024-11-11 02:05:37 -0300

    FIx CORS origin 6

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://linces-voley-sales-back.onrender.com",
+    origin: "https://linces-front.vercel.app",
     credentials: true,
   })
 );
