4321a5a7bd8562fa0bd67f3318c19c44a6efbd21
tmpelay <tmpelay@gmail.com>
2024-11-11 20:34:29 -0300

    Fixed PORT

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import app from "./app.js";
 
-app.listen(process.env.PORT, () => {
-  console.log("Server running on port: ", process.env.PORT);
+const PORT = process.env.PORT | 8080;
+
+app.listen(PORT, () => {
+  console.log("Server running on port: ", PORT);
 });
